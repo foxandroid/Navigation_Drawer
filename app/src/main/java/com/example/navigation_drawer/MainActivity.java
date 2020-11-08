6b5e39a2b0b83070f3116d1d7da6a41e8b1f83f3
robin216 <37136276+robin216@users.noreply.github.com>
2020-11-08 12:51:15 +0530

    First commit

--- app/src/main/java/com/example/navigation_drawer/MainActivity.java
+++ app/src/main/java/com/example/navigation_drawer/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.navigation_drawer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                drawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
     }
 }
